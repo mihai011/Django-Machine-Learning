af9ba95555087f7244b90bf9404427c23a192e93
Mihailescu Mihai <mihaimihai011@gmail.com>
2021-02-13 11:53:51 +0200

    Added code

--- DjangoDL/core/models.py
+++ DjangoDL/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class DeepLearningModel(models.Model):
+
+    model = models.FileField()
+    transformations = models.JSONField(default = dict)
+
+class Inference(models.Model):
+
+    model = models.ForeignKey('DeepLearningModel', on_delete=models.CASCADE)
+    image = models.ImageField()
+
+
+
--- DjangoDL/core/views.py
+++ DjangoDL/core/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
-
+from .forms import InferenceForm, ModelForm
+from .models import Inference, DeepLearningModel
 # Create your views here.
+
+
+def main(request):
+
+    context = {}
+    context['form'] = InferenceForm()
+
+    if request.method == 'POST':
+        form = InferenceForm(request.POST)
+        if form.is_valid():
+            inf = Inference(form)
+            inf.save()
+            return render(request, template_name='main.html', context=context)
+
+    return render(request, template_name='main.html', context=context)
+
+def model(request):
+
+    context = {}
+    context['form'] = ModelForm()
+
+    if request.method == 'POST':
+        form = ModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save(commit=True)
+            return render(request, template_name='form.html', context=context)
+
+    return render(request, template_name='form.html', context=context)
+
+
+
+def inference(request):
+    context = {}
+    context['form'] = InferenceForm()
+
+    if request.method == 'POST':
+        form = InferenceForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save(commit=True)
+            return render(request, template_name='form.html', context=context)
+
+    return render(request, template_name='form.html', context=context)
